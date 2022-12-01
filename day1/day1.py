4c51f259f91240b3f80c90e302baba49eb3380fc
Martin Arroyo <marroyo7775@gmail.com>
2022-12-01 09:33:03 -0500

    Added comments to outline thought process

--- day1/day1.py
+++ day1/day1.py
@@ -2,12 +2,17 @@
 from pathlib import Path
 
 with open(Path.cwd()/'day1/input.txt', 'r', encoding='utf-8') as f:
+    # Take input and break the packs up according to newline characters.
+    # Two newline characters in a row indicate a delimiter between packs
     elf_packs = f.read().replace('\n', ' ').split('  ')
-
+# Convert text to int
 elf_calories = [pack.strip().split(' ') for pack in elf_packs]
 elf_calories = map(lambda calories: [int(cal) for cal in calories], elf_calories)
+# Find the total calories in each pack and sort in reverse order to find the packs
+# with the most calories
 calories_by_pack = [sum(pack) for pack in elf_calories]
 calories_by_pack.sort(reverse=True)
+# Answer questions 1 & 2
 most_calories = max(calories_by_pack)
 top3_total_calories = sum(calories_by_pack[:3])
 
